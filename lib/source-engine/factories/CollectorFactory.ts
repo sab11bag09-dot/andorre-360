991b4da4fd2521901b42b1bfbc21b6a71e87e39c
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-04 16:45:36 +0200

    feat: consolider le routage des collecteurs

--- lib/source-engine/factories/CollectorFactory.ts
+++ lib/source-engine/factories/CollectorFactory.ts
@@ -3,8 +3,11 @@ import {
   SourceCollectionMode,
 } from "@/lib/generated/prisma/client";
 
+import { AndorraBusinessCollector } from "../collectors/AndorraBusinessCollector";
 import { Collector } from "../collectors/Collector";
 import { HtmlCollector } from "../collectors/HtmlCollector";
+import { MeteoAlertsCollector } from "../collectors/MeteoAlertsCollector";
+import { MobilitatCollector } from "../collectors/MobilitatCollector";
 import { RssCollector } from "../collectors/RssCollector";
 import { CollectorFactoryInterface } from "./CollectorFactoryInterface";
 
@@ -16,8 +19,28 @@ export class CollectorFactory
       case SourceCollectionMode.RSS:
         return new RssCollector();
 
-      case SourceCollectionMode.HTML:
+      case SourceCollectionMode.HTML: {
+        const url = new URL(source.url);
+
+        if (url.hostname === "www.mobilitat.ad") {
+          return new MobilitatCollector();
+        }
+
+        if (
+          url.hostname === "www.meteo.ad" &&
+          url.pathname.toLowerCase() === "/alertes"
+        ) {
+          return new MeteoAlertsCollector();
+        }
+
+        if (
+          url.hostname === "www.andorrabusiness.com"
+        ) {
+          return new AndorraBusinessCollector();
+        }
+
         return new HtmlCollector();
+      }
 
       default:
         throw new Error(
