9ecbaf1ce8158919b0e5752555eb5a6b833acba5
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-10 11:40:26 +0200

    style: reduce society bon a savoir block

--- components/SocietePageV2.tsx
+++ components/SocietePageV2.tsx
@@ -234,7 +234,7 @@ export default async function SociétéPageV2() {
 
             {bonASavoir && (
               <div className="mt-auto">
-                <article className="flex h-[540px] flex-col overflow-hidden rounded-xl border-t-2 border-yellow-500 bg-zinc-900 p-6">
+                <article className="flex h-[420px] flex-col overflow-hidden rounded-xl border-t-2 border-yellow-500 bg-zinc-900 p-6">
                   <Link
                     href={`/article/${bonASavoir.slug}`}
                     className="block transition hover:text-yellow-500"
