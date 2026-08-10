b8b695b39ea1e75e3544022c94227acbc94de8b5
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-10 20:52:02 +0200

    style: align all sports footer blocks

--- components/SportsPageV2.tsx
+++ components/SportsPageV2.tsx
@@ -274,7 +274,7 @@ export default async function SportsPageV2() {
             {bonASavoir && (
               <Link
                 href={`/article/${bonASavoir.slug}`}
-                className="block mt-24 lg:h-[400px]"
+                className="block mt-8 lg:h-[400px]"
               >
                 <article className="h-[400px] overflow-hidden rounded-xl border-t-2 border-yellow-500 bg-zinc-900 transition hover:bg-zinc-800">
                   {(bonASavoir.image || fallbackImage) ? (
