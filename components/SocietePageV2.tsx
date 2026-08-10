de5797c0883475d783dafd301d4786ddc7a431a0
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-10 11:38:39 +0200

    fix: remove society footer gap

--- components/SocietePageV2.tsx
+++ components/SocietePageV2.tsx
@@ -123,7 +123,7 @@ export default async function SociétéPageV2() {
             {bottomCard && (
               <Link
                 href={`/article/${bottomCard.slug}`}
-                className="mt-10 block lg:mt-auto"
+                className="mt-10 block"
               >
                 <article className="grid h-[540px] overflow-hidden rounded-xl border border-gray-800 transition hover:border-yellow-500 md:grid-cols-4">
                   <div className="relative md:col-span-3">
