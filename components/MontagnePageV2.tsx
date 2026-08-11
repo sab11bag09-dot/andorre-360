0aff58d6d45886a27ac52a4c5238d39589031a14
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-11 22:33:04 +0200

    style: enlarge mountain question image

--- components/MontagnePageV2.tsx
+++ components/MontagnePageV2.tsx
@@ -171,7 +171,7 @@ export default async function MontagnePageV2() {
                 className="block"
               >
                 <article className="overflow-hidden rounded-xl border border-gray-800 transition hover:border-yellow-500">
-                  <div className="relative h-40">
+                  <div className="relative h-56">
                     <SafeImage
                       src={questionArticle.image || fallbackImage}
                       alt={questionArticle.title}
