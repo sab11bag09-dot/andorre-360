73a31057ceb5cc7e67b7e22ffd9d1daacc0a1e72
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-14 07:19:02 +0200

    layout: reduit le papier ventral pour remonter les breves societe

--- components/SocietePageV2.tsx
+++ components/SocietePageV2.tsx
@@ -65,7 +65,7 @@ export default async function SociétéPageV2() {
 
             {mainArticle && (
               <Link href={`/article/${mainArticle.slug}`} className="block">
-                <article className="flex h-[850px] flex-col overflow-hidden rounded-xl border border-gray-800 transition hover:border-yellow-500">
+                <article className="flex h-[700px] flex-col overflow-hidden rounded-xl border border-gray-800 transition hover:border-yellow-500">
                   <div className="relative h-[500px] shrink-0">
                     <SafeImage
                       src={mainArticle.image}
