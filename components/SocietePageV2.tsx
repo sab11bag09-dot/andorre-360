fff5715e746d2222b1cbf4638a8965c69229098a
sab11bag09-dot <sab11.bag09@gmail.com>
2026-08-16 08:12:19 +0200

    fix: uniformiser la hauteur des photos du pied Société

--- components/SocietePageV2.tsx
+++ components/SocietePageV2.tsx
@@ -152,7 +152,7 @@ export default async function SociétéPageV2() {
                     className="block lg:h-full"
                   >
                     <article className="h-full overflow-hidden rounded-xl border border-gray-800 transition hover:border-yellow-500 lg:flex lg:flex-col">
-                      <div className="relative h-48 lg:h-auto lg:min-h-48 lg:flex-1">
+                      <div className="relative h-48 max-h-48">
                         <SafeImage
                           src={bottomCard.image}
                           alt={bottomCard.title}
@@ -181,7 +181,7 @@ export default async function SociétéPageV2() {
                     className="block lg:h-full"
                   >
                     <article className="h-full overflow-hidden rounded-xl border border-gray-800 transition hover:border-yellow-500 lg:flex lg:flex-col">
-                      <div className="relative h-48 lg:h-auto lg:min-h-48 lg:flex-1">
+                      <div className="relative h-48 max-h-48">
                         <SafeImage
                           src={secondBottomCard.image}
                           alt={secondBottomCard.title}
